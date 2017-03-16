3aad33cfea8a512e838d453f43eeb6f491fb37ad
Rachel Miller <millerrach@gmail.com>
2017-03-16 12:02:08 +0000

    Multiple selection dropdown finished

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,37 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            selected: []
+            selected: [],
         }
     }
     selection(selected) {
         this.setState({
             selected
         });
-        console.log(selected, this.state.selected);
     }
     render() {
-        let tableHead = ['Dish', 'Origin', 'Details', 'Price'];
         let data = [
             {name: 'Gelato', origin: 'Italy', details: 'Organic', price: '4 Euro'},
             {name: 'Hamburger', origin: 'Germany', details: 'Grass Fed', price: '10 Euro'},
             {name: 'Pizza', origin: 'Italy', details: 'Organic', price: '14 Euro'},
             {name: 'Sausage', origin: 'Austria', details: 'Organic', price: '8 Euro'}
         ]
-        let tableHeader = tableHead.map((item, i) => <th key={i} className={item.toLowerCase()}>{item}</th>);
+        let select = this.state.selected;
+        let one = true;
+        let two = true;
+        let three = true;
+        if(this.state.selected.length > 0) {
+            one = select.find((element) => element === '1');
+            two = select.find((element) => element === '2');
+            three = select.find((element) => element === '3');
+        }
         let tableData = data.map((item, i) => {
             return (
-                <tr key={i}>
+                <tr key={i} className="tableBody">
                     <td>{item.name}</td>
-                    <td className='origin'>{item.origin}</td>
-                    <td className='details'>{item.details}</td>
-                    <td>{item.price}</td>
+                    {one ? <td className='origin'>{item.origin}</td> : null}
+                    {two ? <td className='details'>{item.details}</td> : null}
+                    {three ? <td>{item.price}</td> : null}
                 </tr>
             )
         })
@@ -42,7 +48,10 @@ class App extends Component {
                         <table>
                             <thead>
                             <tr>
-                                {tableHeader}
+                                <th>Dish</th>
+                                {one ? <th>Origin</th> : null}
+                                {two ? <th>Details</th> : null}
+                                {three ? <th>Price</th> : null}
                             </tr>
                             </thead>
                             <tbody>
